8b76aba2c6fb5e5861ccebcafe787086e15e9db7
MrFix0r <mrfix0r@gmail.com>
2019-03-22 16:53:05 +0300

    Подготовил к отправке код, осталось добавить комментарии

--- TestWebApp/TestWebApp/Startup.cs
+++ TestWebApp/TestWebApp/Startup.cs
@@ -20,8 +20,7 @@ namespace TestWebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddEntityFrameworkNpgsql().AddDbContext<MailContext>(opt => 
+            services.AddEntityFrameworkNpgsql().AddDbContext<MailContext>(opt =>
             opt.UseNpgsql(Configuration.GetConnectionString("PostgresConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddSingleton<IConfiguration>(Configuration);

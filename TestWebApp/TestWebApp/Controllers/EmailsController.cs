09d888675da7cd71a7b2d1ad662e58a8f0fed7f9
MrFix0r <mrfix0r@gmail.com>
2019-03-22 13:26:06 +0300

    Налажена отправка эмейлов по SMTP

---
+++ TestWebApp/TestWebApp/Controllers/EmailsController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TestWebApp.Models;
+using TestWebApp.Services;
+
+namespace TestWebApp.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("api/mails")]
+    [ApiController]
+    public class EmailsController : ControllerBase
+    {
+        EmailService emailService = new EmailService();
+
+        private readonly MailContext _context;
+
+        // GET api/mails
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/mails
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        // POST api/mails
+        [HttpPost]
+        public async Task<IActionResult> PostEmailItemAsync(Email email)
+        {
+            Random rnd = new Random();
+            email.Id = rnd.Next();
+            await emailService.SendEmailAsync(email.Recipients, email.Subject, email.Body);
+            
+            //_context.Emails.Add(email);
+            //_context.SaveChanges();
+            //await _context.SaveChangesAsync();
+
+            return Ok(email);
+            //CreatedAtAction(nameof(GetTodoItem), new { id = item.Id }, item);
+        }
+
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ TestWebApp/TestWebApp/Services/EmailService.cs
@@ -0,0 +1,38 @@
+﻿using MimeKit;
+using MailKit.Net.Smtp;
+using System.Threading.Tasks;
+using TestWebApp.Models;
+
+namespace TestWebApp.Services
+{
+    public class EmailService
+    {
+        public async Task SendEmailAsync(string[] recipients, string subject, string message)
+        {
+            var emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress("Тестовое задание Роспартнер"));
+            foreach (string recipient in recipients)
+            {
+                emailMessage.To.Add(new MailboxAddress("", recipient));
+            }
+            emailMessage.Subject = subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = message
+            };
+
+            using (var client = new SmtpClient())
+            {
+                //TODO вынести в отдельный файл конфигурации
+                await client.ConnectAsync("smtp.gmail.com", 587, false);
+                await client.AuthenticateAsync("mrfix0r@gmail.com", "qarynelffgoqaiip");
+                await client.SendAsync(emailMessage);
+
+                await client.DisconnectAsync(true);
+            }
+        }
+
+
+    }
+}
